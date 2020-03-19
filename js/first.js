f1ea949dfcea2e625bba4091fec57d64a8635e92
Walsch1 <Walschbrittany@gmail.com>
2020-03-19 13:46:48 -0400

    IDK trying my best
    tried adding javascript and start of story line

--- js/first.js
+++ js/first.js
@@ -1,3 +1,11 @@
+$(".button").click(function() {
+  console.log($("first"));
+  if ( $("#first")[0].innerHTML === "RIP") {
+      $("#first").html("");
+      $("body").removeClass("killed");
+  }
+});
+
 $("#help").click(function() {
   $("#first").append("You throw a rock at the car");
 });
